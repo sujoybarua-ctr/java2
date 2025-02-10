d8958401b201d94b341ba0af2a356c1a404e7f0a
sujoybarua-ctr <sujoybinary@gmail.com>
2025-02-10 10:26:08 -0800

    learning 2D arryas

---
+++ TwoDarray.java
@@ -0,0 +1,9 @@
+public class TwoDarray {
+    public static void main(String[] args){
+        int [][] a= new int [2][3];
+    a[0][0]=1;
+    a[2][2]=78;
+    System.out.println(a[0][0]);
+    System.out.println(a[2][2]);
+    }
+}
