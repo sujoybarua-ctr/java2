c9ed7c8237fb70776d0e2d9f990c0cbc95efe32b
sujoybarua-ctr <sujoybinary@gmail.com>
2025-02-14 07:14:40 -0800

    learning the input syntax for interger and string

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args){
+        Scanner a=new Scanner(System.in);
+        System.out.print("enter your name:");
+String name=a.nextLine();
+System.out.println("youre "+ name +" ?");
+System.out.print(name+ ", tell me what your age is :");
+int age=a.nextInt();
+System.out.print("your age is "+ age+" ? ");
+    }
+}
